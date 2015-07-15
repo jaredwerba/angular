1ceeb5ed0c06eaba186a177ccf16f9af842df292
jaredwerba <jwerba@gmail.com>
2015-07-15 19:13:41 -0400

    added dislike button

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -7,22 +7,25 @@ app.controller('MainController',['$scope', function($scope) {
     price: 19,
     pubdate: new Date('2014', '03', '08'),
     cover: 'img/the-book-of-trees.jpg',
-    likes: 0
+    likes: 0,
+    dislikes: 0
   },
   {
     name: 'Program or be Programmed',
     price: 8,
     pubdate: new Date('2013', '08', '01'),
     cover: 'img/program-or-be-programmed.jpg',
-    likes: 0
+    likes: 0,
+    dislikes: 0
   },
-
+    
     {
     name: 'legend of drizzt: homeland',
     price: 11,
     pubdate: new Date('1995', '03', '20'),
     cover: 'http://ecx.images-amazon.com/images/I/51RWkb7OlGL._SY344_BO1,204,203,200_.jpg',
-    likes: 0
+    likes: 0,
+    dislikes: 0
     },
 
     {
@@ -30,10 +33,15 @@ app.controller('MainController',['$scope', function($scope) {
       price: 11,
       pubdate: new Date('1990', '12', '29'),
       cover: 'http://ecx.images-amazon.com/images/I/51MHC5A5MHL.jpg',
-    likes: 0
+    likes: 0,
+    dislikes: 0
     }
 ];
 $scope.plusOne = function(index){
 $scope.products[index].likes += 1;
+};
+
+$scope.minusOne = function(index){
+  $scope.products[index].dislikes +=1;
 };
 }]);
